cef9080282f679543b9d10c2fd136cefb94117cc
王庆军 <w21976169@163.com>
2020-05-23 11:39:01 +0800

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,87 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+		<Layout>
+			<div class="container">
+				<Header class="nav">
+					<div class="nav-inner">
+						<router-link to="/" class="nav-title"> WYNNYO BLOG</router-link>
+						<router-link to="/" class="nav-item"> <a-icon type="home" />首页</router-link>
+						<router-link class="nav-item" to="/about"><a-icon type="appstore" />分类</router-link>
+						<router-link class="nav-item" to="/about"><a-icon type="gift" />归档</router-link>
+						<router-link class="nav-item" to="/about"><a-icon type="tags" />标签</router-link>
+						<router-link class="nav-item" to="/about"><a-icon type="github" />开源</router-link>
+						<router-link class="nav-item" to="/about"><a-icon type="meh" />关于</router-link>
+					</div>
+				</Header>
+				<Content>
+					<router-view />
+				</Content>
+				<Footer
+					><a-icon type="copyright" />2020&nbsp;
+					<a-icon type="heart" />
+					&nbsp;wynnyo&nbsp;|&nbsp;赞赏</Footer
+				>
+			</div>
+		</Layout>
+	</div>
 </template>
 
 <style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: 'Lato', 'PingFang SC', 'Microsoft YaHei', sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.container {
+	width: 100%;
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.nav {
+	padding: 30px;
+	font-size: 12px;
+	background: #2c3e50;
+	height: 85px;
+	.nav-inner {
+		margin: auto;
+		background: #2c3e50;
+		max-width: 700px;
+		text-align: right;
+	}
+	.nav-title {
+		font-size: 22px;
+		float: left;
+	}
+	.nav-item {
+		margin-top: 20px;
+		color: #fff;
+		display: inline-block;
+		padding-right: 10px;
+		width: 68px;
+		height: 26px;
+		line-height: 26px;
+		i {
+			display: inline-block;
+			margin: 0 10px;
+		}
+	}
+	.nav-item:hover {
+		color: #000;
+		background: #fff;
+	}
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+footer {
+	background: #2c3e50;
+	font-size: 14px;
+	font-weight: 600;
+	color: #fff;
+	height: 50px;
+	line-height: 50px;
+	bottom: 0;
+	position: absolute;
+	width: 100%;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+
 Vue.config.productionTip = false;
 
+Vue.use(Antd);
+
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
+	router,
+	store,
+	render: (h) => h(App)
 }).$mount('#app');
